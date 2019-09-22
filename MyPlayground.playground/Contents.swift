3ab5cb1280849df60bc458a037928f7257fec579
woshahua <wohangye@gmail.com>
2019-09-22 23:02:34 +0900

    update

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -138,4 +138,123 @@ func addElement(ball: String){
 
 addElement(ball: "lil")
 
+// 单例
+class SingleTon{
+    static let shared = SingleTon()
+    private init(){}
+}
 
+import Foundation
+
+// 结构体是传递值，class是传递引用
+
+struct Point{
+    var x = 10
+    var y = 10
+}
+
+var a = Point()
+print(a)
+var b = a
+b.x += 10
+print(b)
+print(a)
+
+class PointClass{
+    var x = 10
+    var y = 10
+}
+
+let ac = PointClass()
+print(ac)
+let bc = ac
+bc.x += 10
+print(ac.x)
+print(bc.x)
+
+// didset
+struct TestDidSet{
+    var age = 10{
+        willSet{
+            print("\(age) is, new value is \(newValue)" )
+        }
+        didSet{
+            print("\(oldValue) is, new value is \(age)" )
+        }
+    }
+}
+
+var t = TestDidSet()
+t.age = 100
+
+class A{
+    var b: B?
+    deinit {
+        print("A released")
+    }
+}
+
+class B{
+    weak var a: A?
+    deinit {
+        print("B released")
+    }
+}
+
+var aclos: A? = A()
+var bclos: B? = B()
+aclos?.b = bclos
+bclos?.a = aclos
+
+aclos = nil
+//bclos = nil
+
+var aStr: String?
+
+// list is value in swift so
+var xlist = [1, 2, 3]
+var ylist = xlist
+xlist.append(5)
+// ylist wont change
+ylist
+
+
+// 重载
+func logger<View: UIView>(_ view: View){
+    print("it's \(type(of: view)), frame:\(view.frame)")
+}
+
+func logger(_ view: UILabel){
+    let text = view.text ?? "(empty)"
+    print("it's UILabel, text is \(text)")
+}
+
+let label = UILabel(frame: CGRect(x: 10, y: 10, width: 10, height: 10))
+logger(label)
+
+let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+logger(button)
+
+// currying
+func addTo2(_ adder: Int)-> (Int) -> Int {
+    return {
+        num in
+        return num + adder
+    }
+}
+
+var addTo22 = addTo2(2)
+addTo22(10)
+// a function need a return value is for this curryig
+// for currying this is simply, return a function.
+// and return a function in closure
+
+// test map
+var sequence: [Int] = [1,2,3,4,5,6]
+// can we set a list that has diffrent type of element
+var sequenceM: [Any?] = [1, "a", 3, "b", nil] // need to set type as any or not
+
+sequence = sequence.map{seq in seq*seq}
+sequence
+// map can follow with a closure function
+// 
